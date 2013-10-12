2f6e6014d7433c96f8b3b89f34f96f0544122d1c
Anthony Day <day1118@gmail.com>
2013-10-12 18:41:42 +1100

    Initial commit

---
+++ HW_Motor_Test.ino
@@ -0,0 +1,60 @@
+/*
+  Hockey Warrior - Encoder Test
+  Tests the Encoder Sensors & prints the results. Can also run the motors.
+ */
+ 
+  #include "config.h"
+
+  #define MOTOR_L_ENABLE_ON
+  #define MOTOR_R_ENABLE_ON
+  #define MOTOR_B_ENABLE_ON
+
+  #define MOTOR_L_SPEED     120
+  #define MOTOR_R_SPEED     120
+  #define MOTOR_B_SPEED     80
+
+  #define MOTOR_L_FORWARDS    true
+  #define MOTOR_R_FORWARDS    true
+  #define MOTOR_B_FORWARDS    true 
+
+void setup() {
+	// Set IR pins as outputs
+  
+  #ifdef MOTOR_L_ENABLE_ON
+    pinMode(MOTOR_L_A_PIN, OUTPUT);
+    pinMode(MOTOR_L_B_PIN, OUTPUT);  
+    pinMode(MOTOR_L_ENABLE_PIN, OUTPUT);
+
+    digitalWrite(MOTOR_L_A_PIN, MOTOR_L_FORWARDS);
+    digitalWrite(MOTOR_L_B_PIN, !MOTOR_L_FORWARDS);
+    analogWrite(MOTOR_L_ENABLE_PIN, MOTOR_L_SPEED);
+  #endif
+
+#ifdef MOTOR_R_ENABLE_ON
+    pinMode(MOTOR_R_A_PIN, OUTPUT);
+    pinMode(MOTOR_R_B_PIN, OUTPUT);  
+    pinMode(MOTOR_R_ENABLE_PIN, OUTPUT);
+
+    digitalWrite(MOTOR_R_A_PIN, MOTOR_R_FORWARDS);
+    digitalWrite(MOTOR_R_B_PIN, !MOTOR_R_FORWARDS);
+    analogWrite(MOTOR_R_ENABLE_PIN, MOTOR_R_SPEED);
+  #endif
+
+  #ifdef MOTOR_B_ENABLE_ON
+    pinMode(MOTOR_B_A_PIN, OUTPUT);
+    pinMode(MOTOR_B_B_PIN, OUTPUT);  
+    pinMode(MOTOR_B_ENABLE_PIN, OUTPUT);
+
+    digitalWrite(MOTOR_B_A_PIN, MOTOR_B_FORWARDS);
+    digitalWrite(MOTOR_B_B_PIN, !MOTOR_B_FORWARDS);
+    analogWrite(MOTOR_B_ENABLE_PIN, MOTOR_B_SPEED);
+  #endif
+
+  Serial.begin(115200);      // open the serial port at 9600 bps:
+
+  DEBUG_PRINT("Starting...");
+}
+
+void loop() {
+	delay(1);
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+#Hockey Warrior
+This project is for ECE3091 & consists of building a robot that plays hockey.
+The rules & deatils are outlined [here] (http://www.ecse.monash.edu.au/twiki/bin/view/ECE3091HR/WebHome)
+
+##Motor Test
+This code rotates all motors to confirm that they are working.
+
