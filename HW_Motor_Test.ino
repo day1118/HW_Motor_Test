1a0e1844e8efc0670641f7b914f9aee8a3d1285e
Anthony Day <day1118@gmail.com>
2013-10-13 20:41:12 +1100

    Added ServoTest code
    Added ServoTest code to determine how well the servos operated with the
    Motors running.

--- HW_Motor_Test.ino
+++ HW_Motor_Test.ino
@@ -4,6 +4,7 @@
  */
  
   #include "config.h"
+  #include <Servo.h> 
 
   #define MOTOR_L_ENABLE_ON
   #define MOTOR_R_ENABLE_ON
@@ -16,6 +17,18 @@
   #define MOTOR_L_FORWARDS    true
   #define MOTOR_R_FORWARDS    true
   #define MOTOR_B_FORWARDS    true 
+ 
+Servo servoF;  // create servo object to control a servo 
+Servo servoB;
+Servo servoK;
+
+int servoFPos;
+int servoBPos;
+int servoKPos;
+
+int servoState = 0;
+int delayTime = 1000;
+
 
 void setup() {
 	// Set IR pins as outputs
@@ -50,11 +63,64 @@ void setup() {
     analogWrite(MOTOR_B_ENABLE_PIN, MOTOR_B_SPEED);
   #endif
 
+  servoF.attach(SERVO_FRONT_PIN, SERVO_FRONT_MIN_PWM, SERVO_FRONT_MAX_PWM);  // attaches the servo on pin 9 to the servo object 
+  servoB.attach(SERVO_BACK_PIN, SERVO_BACK_MIN_PWM, SERVO_BACK_MAX_PWM);
+  servoK.attach(SERVO_KICK_PIN, SERVO_KICK_MIN_PWM, SERVO_KICK_MAX_PWM);
+
+
   Serial.begin(115200);      // open the serial port at 9600 bps:
 
   DEBUG_PRINT("Starting...");
 }
 
 void loop() {
-	delay(1);
+  PLOT("servoState", servoState);
+
+   switch(servoState)
+   {
+     case 0:
+      servoFPos = SERVO_FRONT_UP;
+      servoBPos = SERVO_BACK_UP;
+      servoKPos = SERVO_KICK_UP;
+      break;
+     case 1:
+      servoFPos = SERVO_FRONT_UP;
+      servoBPos = SERVO_BACK_UP;
+      servoKPos = SERVO_KICK_DOWN;
+      break;
+     case 2:
+      servoFPos = SERVO_FRONT_UP;
+      servoBPos = SERVO_BACK_UP;
+      servoKPos = SERVO_KICK_UP;
+      break;
+     case 3:
+      servoFPos = SERVO_FRONT_DOWN;
+      servoBPos = SERVO_BACK_UP;
+      servoKPos = SERVO_KICK_UP;
+      break;
+     case 4:
+      servoFPos = SERVO_FRONT_UP;
+      servoBPos = SERVO_BACK_UP;
+      servoKPos = SERVO_KICK_UP;
+      break;
+     case 5:
+      servoFPos = SERVO_FRONT_UP;
+      servoBPos = SERVO_BACK_DOWN;
+      servoKPos = SERVO_KICK_UP;
+      break;
+   }
+
+    servoF.write(servoFPos);
+    servoB.write(servoBPos);
+    servoK.write(servoKPos);
+
+   PLOT("servoFPos", servoFPos);
+   PLOT("servoBPos", servoBPos);
+   PLOT("servoKPos", servoKPos);
+
+   servoState++;
+   if(servoState > 5)
+     servoState = 0;
+
+    delay(delayTime);
 }
